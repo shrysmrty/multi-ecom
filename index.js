25da3c3c4fea84091a1f8149009f971532983316
shrysmrty <87909589+shrysmrty@users.noreply.github.com>
2024-11-28 15:41:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+// Add to Cart Button Functionality
+document.querySelectorAll('.add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+      alert('Item added to cart!');
+    });
+  });
+  
